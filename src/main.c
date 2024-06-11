bf94330119473c20ed17b2c1537b252b2e884a4d
Allan Norgaard <allannk@gmail.com>
2024-06-11 14:11:15 +0200

    update to build

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
+// #include <zephyr/logging/log.h>
 
-void main(void) {
-    LOG_INF("Booted MCU");
+int main(void) {
+    // LOG_INF("Booted MCU");
+    __asm("nop");
 }
 
 // K_THREAD_DEFINE(heartbeat_tid, 1024, heartbeat_fn, NULL, NULL, NULL, 5, 0, 500);
